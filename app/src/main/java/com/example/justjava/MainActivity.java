155dfe88075956f4d4c84285d7c726b7747edc2a
Andrew <andrew_847@icloud.com>
2021-02-11 12:19:12 -0500

    Fixed Java Code, Added bracket

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -9,11 +9,13 @@
 package com.example.justjava;
 
 
-
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.TextView;
+
 import java.text.NumberFormat;
 
 /**
@@ -31,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
-        displayPrice( number: 2 * 5);
+        display(2);
+        displayPrice(2 * 5);
     }
 
     /**
@@ -41,10 +43,13 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-/**
- * This method displays the given price on the screen.
- */
-        private void displayPrice(int number) {
+        /**
+         * This method displays the given price on the screen.
+         */
+    }
+        private void displayPrice ( int number){
             TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
             priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
         }
+
+}
