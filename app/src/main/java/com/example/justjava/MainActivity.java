056ed0a038ca51888c0336037f952548bb869d6d
Andrew <andrew_847@icloud.com>
2021-03-07 16:25:30 -0500

    Lesson 6 - String Data Type

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -57,9 +57,12 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int numberOfCoffees = quantity;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 5);
+        String priceMessage = "Free";
+        displayMessage(priceMessage);
+        /**
+         *         int numberOfCoffees = quantity;
+         *         displayPrice(numberOfCoffees * 5);
+         */
     }
 
     /**
@@ -76,5 +79,12 @@ public class MainActivity extends AppCompatActivity {
             TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
             priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
         }
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
 
 }
