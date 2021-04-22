44f64de59c0a33476a556e03175f5fc4861476f7
Andrew <andrew_847@icloud.com>
2021-04-22 13:19:44 -0400

    3.2 - Defined Display Quantity Method

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,21 +1,11 @@
-/**
- * IMPORTANT: Make sure you are using the correct package name.
- * This example uses the package name:
- * package com.example.android.justjava
- * If you get an error when copying this code into Android studio, update it to match the package name found
- * in the project's AndroidManifest.xml file.
- **/
-
 package com.example.justjava;
 
 
+
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
 import android.widget.TextView;
-
 import java.text.NumberFormat;
 
 /**
@@ -23,60 +13,60 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     *Declare Global Variables Here
-     */
-
-    int quantity = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * This method is called when the Increment/Plus button is clicked
-     */
-    public void increment(View view) {
-        quantity++;
-        display(quantity);
-    }
 
     /**
-     * This method is called when the Decrement/Minus button is clicked
+     * Declare Global Variable Here
      */
-    public void decrement(View view)  {
-        if (quantity>0) {
-            quantity--;
-            display(quantity);
-        }
-    }
+    int quantity = 0;
+
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
         int price = quantity * 5;
-        String priceMessage = "Total: $" + price;
-        priceMessage = priceMessage + "\nThank You!";
+        String priceMessage = "thank you for ordering" + quantity + "coffees! \n Amount Due: $" + price; //I used the escape key\n
+        priceMessage = priceMessage + "\n\nYour order will be right up!";
         displayMessage(priceMessage);
+
     }
+
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void (int number) {
+        private void displayQuantity(int number)
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-        /**
-         * This method displays the given price on the screen.
-         */
     }
-    private void displayPrice ( int number){
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
+
+    public void increment(View view) {
+        quantity++;
+        displayQuantity(quantity);
+    }
+
+    public void decrement(View view) {
+        if (quantity > 0) {
+            quantity--;
+            displayQuantity(quantity);
+        }
+    }
+
     /**
      * This method displays the given text on the screen.
      */
@@ -84,5 +74,4 @@ public class MainActivity extends AppCompatActivity {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
     }
-
 }
