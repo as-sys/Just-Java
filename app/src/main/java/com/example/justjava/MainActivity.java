2b5c10b9edbe2b6c80d215db299778e2700ef80c
Andrew <andrew_847@icloud.com>
2021-04-22 13:20:43 -0400

    3.5 Changed Numbers of Coffees

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -42,9 +42,9 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given quantity value on the screen.
      */
     private void (int number) {
-        private void displayQuantity(int number)
+        private void displayQuantity(int numberOfCoffees)
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + numberOfCoffees);
     }
 
     /**
